8cfd743bdf6dbdc0d919d05b82d4547df819cbf6
Steve Larsen <slarsen-3@att.net>
2020-09-11 12:33:28 -0500

    GradeBook.py

---
+++ Python/GradeBook.py
@@ -0,0 +1,17 @@
+last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
+
+subjects = ['physics', 'calculus', 'poetry', 'history']
+
+scores = [98, 97, 85, 88]
+subjects.append('computer science')
+scores.append(100)
+
+gradebook = list(zip(subjects, scores))
+
+gradebook.append(('visual arts', 93))
+
+print(list(gradebook))
+print(type(gradebook))
+
+full_gradebook = gradebook + last_semester_gradebook
+print(full_gradebook)
