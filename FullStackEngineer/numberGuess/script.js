853e4ec5f062c8b156b43dd6a202665e282b5d43
Steve Larsen <slarsen-3@att.net>
2020-11-11 17:31:03 -0600

    script.js

--- FullStackEngineer/numberGuess/script.js
+++ FullStackEngineer/numberGuess/script.js
@@ -4,6 +4,27 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 const generateTarget = () => {
-    return Math.floor(Math.random() * 10)
-}
+  return Math.floor(Math.random() * 10);
+};
 
+const compareGuesses = (humanGuess, computerGuess, target) => {
+  if (Math.abs(humanGuess - target) < Math.abs(compputerGuess - target)) {
+    return true;
+  } else if (Math.abs(computerGuess - target) < Math.abs(humanGuess - target)) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const updateScore = (winner) => {
+  if (winner === "human") {
+    return humanScore++;
+  } else {
+    return computerScore++;
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber++;
+};
