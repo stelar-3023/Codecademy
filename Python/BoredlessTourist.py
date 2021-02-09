3f669360aae31569e90f2a642a722a6471944db8
Steve Larsen <slarsen-3@att.net>
2021-02-09 16:27:41 -0600

    updated to pep8 standards

--- Python/BoredlessTourist.py
+++ Python/BoredlessTourist.py
@@ -1,15 +1,20 @@
 destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'Sao Paolo, Brazil', 'Cairo, Egypt']
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
+
 def get_destination_index(destination):
     destination_index = destinations.index(destination)
-    return destination_index    
+    return destination_index
+
+
 print(get_destination_index("Paris, France"))
+
 
 def get_traveler_location(traveler):
     traveler_destination = traveler[1]
     traveler_destination_index = get_destination_index(traveler_destination)
     return traveler_destination_index
+
 
 test_destination_index = get_traveler_location(test_traveler)
 print(test_destination_index)
@@ -21,13 +26,16 @@ attractions = []
 for destination in destinations:
     attractions.append([])
 
+
 def add_attraction(destination, attraction):
     try:
         destination_index = get_destination_index(destination)
         attractions_for_destination = attractions[destination_index].append(attraction)
     except ValueError:
         return
-add_attraction('Los Angeles, USA',['Venice Beach', ['beach']])
+
+
+add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']])
 print(attractions)
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
@@ -41,6 +49,7 @@ add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical sit
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
 print(attractions)
 
+
 def find_attractions(destination, interests):
     destination_index = get_destination_index(destination)
     attractions_in_city = attractions[destination_index]
@@ -49,30 +58,33 @@ def find_attractions(destination, interests):
     for attraction in attractions_in_city:
         possible_attraction = attraction
         attraction_tags = attraction[1]
-    
+
     for interest in interests:
         if interest in attraction_tags:
-            attractions_with_interest.append(possible_attraction[0]) 
+            attractions_with_interest.append(possible_attraction[0])
     return attractions_with_interest
+
 
 la_arts = find_attractions('Los Angeles, USA', ['art'])
 print(la_arts)
+
 
 def get_attractions_for_traveler(traveler):
     traveler_destination = traveler[1]
     traveler_interests = traveler[2]
     traveler_attractions = find_attractions(traveler_destination, traveler_interests)
-    interests_string = ("Hi " + traveler[0] + ", we think you'll like these places around " + traveler_destination + ":")
+    interests_string = (
+                "Hi " + traveler[0] + ", we think you'll like these places around " + traveler_destination + ":")
 
-
-    
-    for i in range (len(traveler_attractions)):
+    for i in range(len(traveler_attractions)):
         if traveler_attractions[-1] == traveler_attractions[i]:
             interests_string += "the " + traveler_attractions[i] + "."
         else:
             interests_string += "the " + traveler_attractions[i] + ","
     return interests_string
-print(get_attractions_for_traveler(['Derek Smill', 'Los Angeles, USA',['art']]))
+
+
+print(get_attractions_for_traveler(['Derek Smill', 'Los Angeles, USA', ['art']]))
 
 """If last attraction in a list, add a period, else add a comma and a space"""
 
